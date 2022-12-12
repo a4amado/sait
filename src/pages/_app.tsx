7a0be77d1df4f8b8944f5ae31cefb61c6ad8e286
Ahmad Addel <84184203+a4addel@users.noreply.github.com>
2022-12-12 13:06:28 +0200

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import { type AppType } from "next/app";
-import { type Session } from "next-auth";
 import "../styles/globals.css";
 import { appWithTranslation } from 'next-i18next';
 import { ChakraProvider } from '@chakra-ui/react'
